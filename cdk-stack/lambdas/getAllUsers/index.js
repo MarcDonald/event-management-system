433543b21be64e5fdbdd9655ec669269bf9fdb20
Marc Donald <marcsdeveloperemail@gmail.com>
2020-10-23 11:13:17 +0100

    Add get all users endpoint

---
+++ cdk-stack/lambdas/getAllUsers/index.js
@@ -0,0 +1,24 @@
+const aws = require('aws-sdk');
+const { USER_POOL_ID } = process.env;
+const Cognito = new aws.CognitoIdentityServiceProvider();
+
+exports.handler = async () => {
+  const users = await Cognito.listUsers({
+    UserPoolId: USER_POOL_ID,
+  }).promise();
+
+  const formattedUsers = users.Users.map((rawUser) => {
+    return {
+      username: rawUser.Username,
+      sub: rawUser.Attributes[0].Value,
+      role: rawUser.Attributes[1].Value,
+      givenName: rawUser.Attributes[2].Value,
+      familyName: rawUser.Attributes[3].Value,
+    };
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(formattedUsers),
+  };
+};
--- cdk-stack/lib/RestEndpoints/UserRestEndpoints.ts
+++ cdk-stack/lib/RestEndpoints/UserRestEndpoints.ts
@@ -11,35 +11,91 @@ export default class UserRestEndpoints {
     cognitoResources: CognitoResources,
     restApiResources: RestApiResources
   ) {
+    const { userPoolId, userPoolArn } = cognitoResources.userPool;
+    const { userPoolClientId } = cognitoResources.apiUserPoolClient;
+    const { api } = restApiResources;
+
+    const addUserHandler = this.createAddUserHandler(
+      scope,
+      userPoolId,
+      userPoolClientId,
+      userPoolArn
+    );
+
+    const getAllUsersHandler = this.createGetAllUsersHandler(
+      scope,
+      userPoolId,
+      userPoolArn
+    );
+
+    api.addRoutes({
+      path: '/users',
+      methods: [HttpMethod.POST],
+      integration: addUserHandler,
+    });
+
+    api.addRoutes({
+      path: '/users',
+      methods: [HttpMethod.GET],
+      integration: getAllUsersHandler,
+    });
+  }
+
+  private createAddUserHandler = (
+    scope: cdk.Construct,
+    userPoolId: string,
+    clientId: string,
+    userPoolArn: string
+  ): LambdaProxyIntegration => {
     const addUserFunc = new Function(scope, 'addUserFunction', {
       runtime: Runtime.NODEJS_12_X,
       handler: 'index.handler',
       functionName: 'EmsAddUser',
       code: Code.fromAsset('./lambdas/addUser'),
       environment: {
-        USER_POOL_ID: cognitoResources.userPool.userPoolId,
-        API_CLIENT_ID: cognitoResources.apiUserPoolClient.userPoolClientId,
+        USER_POOL_ID: userPoolId,
+        API_CLIENT_ID: clientId,
       },
     });
 
-    const addUserPolicyStatement = new PolicyStatement();
-    addUserPolicyStatement.addResources(cognitoResources.userPool.userPoolArn);
-    addUserPolicyStatement.addActions(
+    const policyStatement = new PolicyStatement();
+    policyStatement.addResources(userPoolArn);
+    policyStatement.addActions(
       'cognito-idp:AdminCreateUser',
       'cognito-idp:AdminInitiateAuth',
       'cognito-idp:AdminRespondToAuthChallenge'
     );
 
-    addUserFunc.addToRolePolicy(addUserPolicyStatement);
+    addUserFunc.addToRolePolicy(policyStatement);
 
-    const addUserHandler = new LambdaProxyIntegration({
+    return new LambdaProxyIntegration({
       handler: addUserFunc,
     });
+  };
 
-    restApiResources.api.addRoutes({
-      path: '/users',
-      methods: [HttpMethod.POST],
-      integration: addUserHandler,
+  private createGetAllUsersHandler = (
+    scope: cdk.Construct,
+    userPoolId: string,
+    userPoolArn: string
+  ): LambdaProxyIntegration => {
+    const getAllUsersFunc = new Function(scope, 'getAllUsersFunction', {
+      runtime: Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      functionName: 'EmsGetAllUsers',
+      code: Code.fromAsset('./lambdas/getAllUsers'),
+      environment: {
+        USER_POOL_ID: userPoolId,
+      },
     });
-  }
+
+    const policyStatement = new PolicyStatement();
+    policyStatement.addResources(userPoolArn);
+    policyStatement.addActions('cognito-idp:ListUsers');
+
+    getAllUsersFunc.addToRolePolicy(policyStatement);
+
+    return new LambdaProxyIntegration({
+      handler: getAllUsersFunc,
+    });
+  };
 }
